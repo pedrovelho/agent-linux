5b6d9de70b10361e382060550caf04ae4efa90a5
vjuresch <vjuresch@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-10-21 11:40:38 +0000

    fixed remove_services_fedora in Makefile.am by replacing BUILDROOT with $(DESTDIR) modified the install_services_ubuntu target so it can be used with the deb target modified the install_services_fedora to create and restore a backup of .rpmmacros when building the rpm added deb target in Makefile.am modified paas and paac to use /usr/bin instead of /usr/local/bin added skeleton debian configuration in debian directory regenerated autotools configuration
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/agent/linux/trunk@14585 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ debian/paagent.default.ex
@@ -0,0 +1,10 @@
+# Defaults for paagent initscript
+# sourced by /etc/init.d/paagent
+# installed at /etc/default/paagent by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
