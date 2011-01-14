f184c1b9db7b42d66d4cafa3433344e7d28edb23
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-01-14 09:26:04 +0000

    Version -> 1.0.0
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/agent/linux/branches/1.0.x@18659 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- palinagent/daemon/main.py
+++ palinagent/daemon/main.py
@@ -56,7 +56,7 @@ This is the ProActive Linux agent main entry point
 # The current XML namespace 
 xmlns = "urn:proactive:agent:1.0:linux"
 
-version = "1.0.0-dev"
+version = "1.0.0"
 
 def _parse_config_file(fname):
     '''
